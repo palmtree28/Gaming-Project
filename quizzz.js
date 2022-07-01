ceacfa565534e71bef5702b373f0e16281c45dcb
Jared Monden <jsmonden@gmail.com>
2022-06-30 19:31:26 -0500

    made javaScripts

--- quizzz.js
+++ quizzz.js
@@ -1,12 +1,39 @@
-let inputElement = document.getElementById("frame1");
-//alert (InputElement.value)
+
+function results()
+ {
+//   const quiz= document.getElementById("quiz");
+//   console.log(quiz)
+
+  let q1 = document.querySelector('input[name="1radio"]:checked').value;
+  if (q1==="Mexico"){
+    alert("Correct");
+  }
+  else{
+    alert("incorrect");
+
+  }
+
+//   var radios = document.get
+//   var val="";
+// for(i=0.length= radios.legth;i<length;i++)
+//   const choice1= document.getElementById("choice1");
+//   const btn= documemt.getElementById("results");
+//   var score= 0;
+
+//   var results= document.getElementById  ("choice1");
+//     (let {choice1},correct: true)
+//   for(let {choiceb1},incorrect: false)
+//   for(let {choicec1},incorrect: false)
+//   for(let {choiced1},incorrect: false)
+
+// }
+
+//  {for let btn}, document.write("c");
+//    quiz.style.display="none";
+// results.textContent is= ('${c}');
+
+
+}
+
+    
  
-//Test if input value is "Greece"
-if (inputElement.value = "Greece")
-{
-    alert ("Correct")
-}
-else{
-    alert("Incorrect- This was not actually a temple, but it was named after the goddess Athena")
-    inputElement.style.border= "2px solid red"
-}
